sirdarckcat/ubuntu

commit 6fecc30361b042f0f13bb0fdcaee5c367008be85

UBUNTU: [Packaging] neuter the signing tarball
We now produce the signing tarball from the generate package so we no
longer this to be produced from the main package.  However, in the short
term we wish to produce and publish out empty signing tarballs.  We will
do this until we have cleared out the dists versions.  Later we will
eliminate the tarball from this package entirely.

BugLink: https://bugs.launchpad.net/bugs/2012776
Signed-off-by: Andy Whitcroft <apw@canonical.com>

debian/rules.d/2-binary-arch.mk
@@ -153,7 +153,7 @@ else
 	chmod 600 $(pkgdir_bin)/boot/$(instfile)-$(abi_release)-$*
 endif
 
-ifeq ($(uefi_signed),true)
+ifeq ($(uefi_signed),DISABLED)
 	install -d $(signingv)
 	# gzipped kernel images must be decompressed for signing
 	if [[ "$(kernfile)" =~ \.gz$$ ]]; then \
@@ -167,12 +167,12 @@ ifeq ($(uefi_signed),true)
 			$(signingv)/$(instfile)-$(abi_release)-$*.efi; \
 	fi
 endif
-ifeq ($(opal_signed),true)
+ifeq ($(opal_signed),DISABLED)
 	install -d $(signingv)
 	cp -p $(pkgdir_bin)/boot/$(instfile)-$(abi_release)-$* \
 		$(signingv)/$(instfile)-$(abi_release)-$*.opal;
 endif
-ifeq ($(sipl_signed),true)
+ifeq ($(sipl_signed),DISABLED)
 	install -d $(signingv)
 	cp -p $(pkgdir_bin)/boot/$(instfile)-$(abi_release)-$* \
 		$(signingv)/$(instfile)-$(abi_release)-$*.sipl;

Clome/ubuntu-kernel

commit 0112b81a67ad61b88737026d1b4e534623858fd2

UBUNTU: [Packaging] remove the signing tarball support
All signing tarball support is now removed (previously only disabled)
as all kernels now use generate out of linux-signed to submit signing.

Fixes: 6fecc30361 ("UBUNTU: [Packaging] neuter the signing tarball")
BugLink: https://bugs.launchpad.net/bugs/2012776
Signed-off-by: Dimitri John Ledkov <dimitri.ledkov@canonical.com>
Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

debian/rules.d/2-binary-arch.mk
@@ -157,32 +157,6 @@ else
 		$(pkgdir_bin)/boot/$(instfile)-$(abi_release)-$*
 	chmod 600 $(pkgdir_bin)/boot/$(instfile)-$(abi_release)-$*
 endif
-
-ifeq ($(uefi_signed),DISABLED)
-	install -d $(signingv)
-	# gzipped kernel images must be decompressed for signing
-	if [[ "$(kernfile)" =~ \.gz$$ ]]; then \
-		< $(pkgdir_bin)/boot/$(instfile)-$(abi_release)-$* \
-			gunzip -cv > $(signingv)/$(instfile)-$(abi_release)-$*.efi; \
-		cp -p --attributes-only $(pkgdir_bin)/boot/$(instfile)-$(abi_release)-$* \
-			$(signingv)/$(instfile)-$(abi_release)-$*.efi; \
-		echo "GZIP=1" >> $(signingv)/$(instfile)-$(abi_release)-$*.efi.vars; \
-	else \
-		cp -p $(pkgdir_bin)/boot/$(instfile)-$(abi_release)-$* \
-			$(signingv)/$(instfile)-$(abi_release)-$*.efi; \
-	fi
-endif
-ifeq ($(opal_signed),DISABLED)
-	install -d $(signingv)
-	cp -p $(pkgdir_bin)/boot/$(instfile)-$(abi_release)-$* \
-		$(signingv)/$(instfile)-$(abi_release)-$*.opal;
-endif
-ifeq ($(sipl_signed),DISABLED)
-	install -d $(signingv)
-	cp -p $(pkgdir_bin)/boot/$(instfile)-$(abi_release)-$* \
-		$(signingv)/$(instfile)-$(abi_release)-$*.sipl;
-endif
-
 	install -d $(pkgdir)/boot
 	install -m644 $(builddir)/build-$*/.config \
 		$(pkgdir)/boot/config-$(abi_release)-$*
@@ -840,17 +814,8 @@ ifeq ($(do_cloud_tools),true)
 endif
 
 .PHONY: binary-debs
-binary-debs: signing = $(CURDIR)/debian/$(bin_pkg_name)-signing
-binary-debs: signingv = $(CURDIR)/debian/$(bin_pkg_name)-signing/$(release)-$(revision)
-binary-debs: signing_tar = $(src_pkg_name)_$(release)-$(revision)_$(arch).tar.gz
 binary-debs: binary-perarch $(addprefix binary-,$(flavours))
 	@echo Debug: $@
-ifeq ($(any_signed),true)
-	install -d $(signingv)/control
-	{ echo "tarball"; } >$(signingv)/control/options
-	cd $(signing) && tar czvf ../../../$(signing_tar) .
-	dpkg-distaddfile $(signing_tar) raw-signing -
-endif
 
 build-arch-deps-$(do_flavour_image_package) += $(addprefix $(stampdir)/stamp-install-,$(flavours))
 